d10b67b2b05ffcb827e7fcb507fab4813a1dd5d0
Rodrigo Quiroz <quiroz.rodrigo@gmail.com>
2024-10-20 09:28:38 -0400

    update readme

--- app/firebase.config.js
+++ app/firebase.config.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCFLraETw1hOSG44xPNPqoXnRImhegTJAM",
-  authDomain: "asoblockchain-6f3de.firebaseapp.com",
-  projectId: "asoblockchain-6f3de",
-  storageBucket: "asoblockchain-6f3de.appspot.com",
-  messagingSenderId: "156804356619",
-  appId: "1:156804356619:web:4b00905fc294dfa350c50e",
-  measurementId: "G-CYL7N4DG6V"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Verifica que todas las propiedades estén definidas
